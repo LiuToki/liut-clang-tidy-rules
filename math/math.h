92fb1195af6fa6a84e58e441f1ae77afd1eafe70
LiuToki <ray.usaneko@gmail.com>
2025-03-09 22:18:40 +0900

    お試しで実装

--- math/math.h
+++ math/math.h
@@ -1,7 +1,36 @@
-﻿#include <iostream>
+﻿#include "clang-tidy/ClangTidyCheck.h"
 
-int main()
-{
-    std::cout << "Hi!" << std::endl;
-    return 0;
-}
+#if LLVM_VERSION_MAJOR >= 15
+#define STRING StringRef
+#else
+#define STRING std::string
+#endif
+
+namespace clang::tidy::extra {
+
+struct AllowedFunctionEntry {
+  STRING Name;
+  std::vector<uint> ArgPositions;
+};
+
+class MathCheck : public ClangTidyCheck {
+  const std::vector<AllowedFunctionEntry> AllowedFunctionsList;
+  const bool RemarkPassed;
+  const std::string SkipPatternStr;
+  llvm::Regex SkipPattern;
+
+  static std::vector<AllowedFunctionEntry>
+  parseAllowedFunctions(const STRING &AllowedFunctions);
+  static std::string serializeAllowedFunctions(
+      const std::vector<AllowedFunctionEntry> &AllowedFunctions);
+  bool isAllowedFunctionCall(const CallExpr *CE, const StringLiteral *S) const;
+  bool isAllowedMacroExpansion(const StringRef MacroName) const;
+
+public:
+  I18nStringCheck(StringRef Name, ClangTidyContext *Context);
+  void registerMatchers(ast_matchers::MatchFinder *Finder) override;
+  void check(const ast_matchers::MatchFinder::MatchResult &Result) override;
+  void storeOptions(ClangTidyOptions::OptionMap &Opts) override;
+};
+
+} // namespace clang::tidy::extra
